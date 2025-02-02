be5822c982be986e3a94ae00d3c11f70f9b03c1c
JaNath123 <chiplusb@gmail.com>
2025-02-02 09:18:36 -0800

    Add files via upload

---
+++ StoredProcedureNew.sql
@@ -0,0 +1,11 @@
+---how to run a stored procedure
+---Type execute in a query and drag or copy the stored precedure code in
+--eg.
+
+exec [dbo].[spPullAllergiesData]
+
+/*why stored procedure
+1. it is used for security
+2. To improve code performance
+3. Used for automation process
+4. It is used for loggin process (Troubleshooting)
