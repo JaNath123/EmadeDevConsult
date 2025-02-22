9dc5f35558ea5cd7bc5796136259b6b0fb52e136
JaNath123 <chiplusb@gmail.com>
2025-02-22 09:21:08 -0800

    Add files via upload

--- HealthCareDataAnalysis.sql
+++ HealthCareDataAnalysis.sql
@@ -1,5 +1,14 @@
 Use EmadeDev
 Go
+/**
+						=====================================================================
+						DEVELOPER: MR BEN
+						CREATE DATE:		02/15/2025
+						PROJECT		HEALTHCARE PROJECT
+						DESCRIPTION:	HEALTHCARE PROJECT
+						VERSION: 1
+						====================================================================
+**/
 
 Select *
 FROM[dbo].[healthcare_dataAnalysis]
@@ -58,9 +67,10 @@ ORDER BY MaximumNoOfPatient DESC, Medical_Condition DESC
 
 --8. Find the Rank and Maximum number of medicines recommended to patients based on medical condition partaining to them
 
-SELECT Medication, Max (Medication) AS MaximumNoOfMedicines, dense_Rank() over(order by Max (Medication) DESC, Medication) AS Rank
+SELECT Medical_Condition, Medication, Count (Medication) AS MaximumNoOfMedicines, 
+dense_Rank() over(order by Count (Medication) DESC, Medication) AS Rank
 FROM Healthcare_dataAnalysis
-GROUP BY Medication
+GROUP BY Medication, Medical_Condition
 --HAVING Max(Medication)=Medication
 ORDER BY MaximumNoOfMedicines DESC, Medication DESC
 
@@ -77,25 +87,39 @@ ORDER BY Insurance_provider DESC, MostPreferred DESC
 
 --10. Finding out the most preferred hospital by patients
 
-SELECT Top 5 * --as MostPreferredHospital Dense_Rank() over(order by COUNT(Hospital) DESC Hospital) as Rank
+SELECT Top  10 * (Hospital, Count(Hospital)) as MostPreferredHospital 
 FROM Healthcare_dataAnalysis
---GROUP BY Hospital
-ORDER BY Hospital DESC
---ORDER BY MostPreferredHospital DESC --Hospital DESC
+GROUP BY Hospital
+ORDER BY MostPreferedHospital DESC
+
+Most Preferred Hospitalselect top 1 *from (SELECT Hospital, COUNT(hospital) AS Total FROM HealthcareDataSetGROUP BY Hospital)xORDER BY Total DESC;
 
 --11. Identiying Avarage Billing Amount by Medical_Condition
 
-SELECT Medical_Condition, Round (Avg(Billing_Amount),0) as AverageBilling
+SELECT Medical_Condition, Round (Avg(Billing_Amount),2) as AverageBilling
 FROM Healthcare_dataAnalysis
 GROUP BY Medical_Condition
 ORDER BY AverageBilling DESC , Medical_Condition DESC 
 
+--Note from Class
+-- 11. Identifying Average Billing Amount by Medical Condition.SELECT Medical_Condition, ROUND(AVG(Billing_Amount),2) AS Avg_Billing_AmountFROM HealthcareDataSetGROUP BY Medical_Condition;
+
+--from Edie
+SELECT [Medical Condition], ROUND(AVG(TRY_CAST([Billing Amount] AS FLOAT)),2) AS Average_BillingFROM HealthCareDatasetGROUP BY [Medical Condition]ORDER BY Average_Billing DESC
+
 --12. Finding Billing Amount of Patients admitted and number of days spent in the respective facilities
 
-SELECT Name, Hospital, ROUND (Sum(Billing_Amount),0) AS BillingAmountPatientAdmitted, DATEDIFF(Day,Date_of_Admission, Discharge_Date) AS DaysSpent
+SELECT Name, Hospital, ROUND (Sum(Billing_Amount),2) AS BillingAmountPatientAdmitted,
+DATEDIFF(Day,Date_of_Admission, Discharge_Date) AS DaysSpent
 FROM Healthcare_dataAnalysis
 GROUP BY Hospital,Name, Billing_Amount, Date_of_Admission, Discharge_Date
 ORDER BY BillingAmountPatientAdmitted DESC, DaysSpent DESC
+
+/*from Class
+- 12. Finding Billing Amount of patients admitted and number of days spent in respective hospital.SELECT Medical_Condition, Name, Hospital, DATEDIFF(DAY,Date_of_Admission,Discharge_date) as Number_of_Days, SUM(ROUND(Billing_Amount,2)) OVER(Partition by Hospital ORDER BY Hospital DESC) AS Total_AmountFROM HealthcareDataSetORDER BY Medical_Condition
+*/
+
+
 
 --13. Finding Total number of days spent by patient in a hospital for a given Medical Condition
 
@@ -103,6 +127,11 @@ SELECT Name, Hospital, Medical_Condition, DateDiff(Day,Date_of_Admission, Discha
 FROM Healthcare_dataAnalysis
 --GROUP BY Name
 ORDER BY TotalNumberOfDays ASC
+
+/* FROM CLASS-- 13. Finding Total number of days sepnt by patient in an hospital for given medical conditionSELECT Name, Medical_Condition, ROUND(Billing_Amount,2) as Billing_Amount, Hospital, DATEDIFF(DAY,Date_of_Admission,Discharge_Date) as Total_Hospitalized_daysFROM HealthcareDataSet;
+*/
+
+
 
 
 --14. Finding Hospital which were successful in discharging patients after having test result as 'Normal' with 
@@ -117,6 +146,8 @@ ORDER BY CountOfDays ASC
 --WHERE Test_Result = 'Normal',THEN 'Successful'
 --ORDER BY Hospital Desc, Successful ASC
 
+/* FROM CLASS -- 14. Finding Hospitals which were successful in discharging patients after having test results as 'Normal' with count of days taken to get results to NormalSELECT Medical_Condition, Hospital, DATEDIFF(DAY, Date_of_Admission,Discharge_Date) as Total_Hospitalized_days,Test_resultsFROM HealthcareDataSetWHERE Test_results LIKE 'Normal'ORDER BY Medical_Condition, Hospital
+*/
 
 ---15. Calculate number of blood types of patients which lies between age 20 to 45
 
@@ -126,7 +157,9 @@ GROUP BY Blood_Type, Age
 HAVING Age Between 20 and 45
 ORDER BY Age DESC
 
-
+/* FROM CLASS
+-- 15. Calculate number of blood types of patients which lies betwwen age 20 to 45SELECT Age, Blood_type,COUNT(Blood_Type) as Count_Blood_TypeFROM HealthcareDataSetWHERE AGE BETWEEN 20 AND 45GROUP BY Age, Blood_typeORDER BY Blood_Type DESC;
+*/
 
 --16. Find how many of patient are universal Blood Donor and universal Blood reciever
 
@@ -136,3 +169,7 @@ GROUP BY Blood_Type
 HAVING Blood_Type = 'O+' OR Blood_Type = 'AB+'
 OR Blood_Type = 'O-'OR Blood_Type = 'AB-'
 ORDER BY BloodDonor DESC, BloodReciever DESC
+
+/*
+FROM CLASS
+-- 16. Find how many of patient are Universal Blood Donor and Universal Blood recieverSELECT DISTINCT (SELECT Count(Blood_Type) FROM HealthcareDataSet WHERE Blood_Type IN ('O-')) AS Universal_Blood_Donor, (SELECT Count(Blood_Type) FROM HealthcareDataSet WHERE Blood_Type  IN ('AB+')) as Universal_Blood_recieverFROM HealthcareDataSet;*/SELECT Patients, Blood_Type, Donor_Reciver_Status From( Select Blood_Type, Count(NAME) Patients, Case  when Blood_Type like '%O-%' then 'Universal Blood Donor' when Blood_Type like '%AB+%' THEN 'Universal Blood reciever' ELSE ' ' END Donor_Reciver_Status FROM Emade_dev.[dbo].[healthcare_dataset] Where Blood_Type IN ('O-' , 'AB+') Group by Blood_Type) x
